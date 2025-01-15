3fe185f70e8123e60524c01636b6347ee9d43fa1
scho-vn <vanessa.schoene5@gmail.com>
2025-01-15 13:14:01 +0100

    Update pz_analysis_fe.py
    removed list pipeline

--- pz_analysis_fe.py
+++ pz_analysis_fe.py
@@ -29,65 +29,61 @@ specimen_name='fe'
 side = 'right'
 specimen_type = 'FE'
 
-specimenlist=['fe']
-sidelist=['right']
-for specimen_name in specimenlist:
-    for side in sidelist:
-        data_input = data_input_from_csv_fe(csv_filepath=os.path.join(global_path, 'data_examples', f'{specimen_name}', 'FAT_Input_all_rechts.txt'))
+data_input = data_input_from_csv_fe(csv_filepath=os.path.join(global_path, 'data_examples', f'{specimen_name}', 'FAT_Input_all_rechts.txt'))
 
 
-        #filtered_data = filter_data_input(data_in=data_input, limit=22)
-        filtered_data = filter_data_input(data_in=data_input, limit=(30,170))
-        sum_nodemaps_to_results={}
+#filtered_data = filter_data_input(data_in=data_input, limit=22)
+filtered_data = filter_data_input(data_in=data_input, limit=(30,170))
+sum_nodemaps_to_results={}
 
 
-        input_list=list(filtered_data)
+input_list=list(filtered_data)
 
 
-        for item in input_list:
+for item in input_list:
 
-            analysis = Data_Processing(specimen_name=specimen_name,
-                                       side = side,
-                                       nodemap_name = item,
-                                       specimen_type = specimen_type)
-            mask = analysis.mask_data(crack_tip_x=data_input[item][0],
-                                      crack_tip_y=data_input[item][1],
-                                      strain_treshold = 0.68,
-                                      crack_tip_tolerance = 0.1,
-                                      reduce_x_window = (0,15),
-                                      reduce_y_window = (180,180))
+    analysis = Data_Processing(specimen_name=specimen_name,
+                               side = side,
+                               nodemap_name = item,
+                               specimen_type = specimen_type)
+    mask = analysis.mask_data(crack_tip_x=data_input[item][0],
+                              crack_tip_y=data_input[item][1],
+                              strain_treshold = 0.68,
+                              crack_tip_tolerance = 0.1,
+                              reduce_x_window = (0,15),
+                              reduce_y_window = (180,180))
 
-            evaluate = analysis.evaluate_contours(which_contours = ['Whole'],
-                                                  secondary_crack_treshold=80)
+    evaluate = analysis.evaluate_contours(which_contours = ['Whole'],
+                                          secondary_crack_treshold=80)
 
-            sum_nodemaps_to_results.update(analysis.nodemap_to_results)
+    sum_nodemaps_to_results.update(analysis.nodemap_to_results)
 
-            plotter = Plotter(Result=analysis,
-                              which_contours = ['Whole'])
-            plotter.plot_contour(plot_contour=True,
-                                 plot_extreme_points=True,
-                                 window_x=(2,2),
-                                 window_y=(2,2),)
+    plotter = Plotter(Result=analysis,
+                      which_contours = ['Whole'])
+    plotter.plot_contour(plot_contour=True,
+                         plot_extreme_points=True,
+                         window_x=(2,2),
+                         window_y=(2,2),)
 
-            plotter.plot_contour_on_nodemap(strain_treshold = 0.68,
-                                            num_colors = 120,
-                                            num_colorbars = 3,
-                                            colormap = 'viridis')
+    plotter.plot_contour_on_nodemap(strain_treshold = 0.68,
+                                    num_colors = 120,
+                                    num_colorbars = 3,
+                                    colormap = 'viridis')
 
-            Result_Writer(Result=analysis).write_to_csv()
+    Result_Writer(Result=analysis).write_to_csv()
 
-        summary = sum_results(specimen_name=specimen_name,
-                              side=side,
-                              result_path=os.path.join(global_path, '02_results',f'{specimen_name}', f'{side}',
-                                                       '02_Data_Evaluation'),
-                              delete_single_files=True,
-                              key_index='Crack Tip X[mm]')
-
-        specimen_name_to_results ={f'{specimen_name}_{side}': sum_nodemaps_to_results}
-
-        pickle_output(specimen_name=specimen_name,
+summary = sum_results(specimen_name=specimen_name,
                       side=side,
-                      result_path=analysis.output_path_pickle,
-                      which=specimen_name_to_results)
-        print('done')
+                      result_path=os.path.join(global_path, '02_results',f'{specimen_name}', f'{side}',
+                                               '02_Data_Evaluation'),
+                      delete_single_files=True,
+                      key_index='Crack Tip X[mm]')
+
+specimen_name_to_results ={f'{specimen_name}_{side}': sum_nodemaps_to_results}
+
+pickle_output(specimen_name=specimen_name,
+              side=side,
+              result_path=analysis.output_path_pickle,
+              which=specimen_name_to_results)
+print('done')
 
